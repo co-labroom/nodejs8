28374140a96ef32f995bfaf2b813bb67c31eb6bd
common <unnkoel@gmail.com>
2017-06-13 23:06:23 +0800

    add Assert.js and debugger

---
+++ Assert.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Miss Esc on 2017/6/11.
+ */
+
+const assert = require('assert');
+
+assert.ok(true);
+assert.ok(false, 'it\'s false');
---
+++ debugger.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Miss Esc on 2017/6/13.
+ */
+
+global.x = 5;
+setTimeout(()=> {
+  debugger;
+  console.log('world');
+}, 1000);
+console.log('hello');
